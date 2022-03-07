b41d78a2a9e6eeb410e4b4cfa15e3a9b58541b0c
Cristobal Arellano <cristobal.arellano@mooving.com>
2022-03-07 15:42:21 -0300

    add automapper add main profile implement automapper on RoleService.GetAll

--- src/Core/Security/Application/Service/RoleService.cs
+++ src/Core/Security/Application/Service/RoleService.cs
@@ -1,4 +1,6 @@
-﻿using Core.Domain;
+﻿using AutoMapper;
+using Core.Domain;
+using Core.Security.Domain.Dto;
 using Core.Security.Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using System;
@@ -15,15 +17,18 @@ namespace Core.Security.Application.Service
         public static string PERMISSION_CLAIM_TYPE = "Auth";
         private readonly RoleManager<IdentityRole> _roleManager;
         //private readonly IConfiguration _configuration;
+        private IMapper _mapper;
 
-        public RoleService(RoleManager<IdentityRole> roleManager)
+        public RoleService(RoleManager<IdentityRole> roleManager, IMapper mapper)
         {
-            this._roleManager = roleManager;
+            _roleManager = roleManager;
+            _mapper = mapper;
         }
 
-        public IQueryable<IdentityRole> GetAll()
+        public IQueryable<RoleDto> GetAll()
         {
-            return _roleManager.Roles;
+            var rolesDto = _mapper.Map<List<RoleDto>>(_roleManager.Roles);
+            return rolesDto.AsQueryable();
         }
 
         public async Task<IList<IdentityResult>> AddClaim(string roleId, IList<string> claims)
@@ -32,7 +37,7 @@ namespace Core.Security.Application.Service
             // Test: What happens if the role does not exist. If the claim already exist
             List<IdentityResult> results = new List<IdentityResult>();
             var role = await _roleManager.FindByIdAsync(roleId);
-            foreach(var claim in claims)
+            foreach (var claim in claims)
             {
                 var result = await _roleManager.AddClaimAsync(role, new Claim(PERMISSION_CLAIM_TYPE, claim));
                 results.Add(result);
